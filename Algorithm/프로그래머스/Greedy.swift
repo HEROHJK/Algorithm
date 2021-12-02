6277560f32ce1faa7a8f2c7f79e98fdda5b2f89a
김호종 <herohjk@herohjk.com>
2021-12-02 12:50:38 +0900

    Update Tuple

--- Algorithm/프로그래머스/Greedy.swift
+++ Algorithm/프로그래머스/Greedy.swift
@@ -95,3 +95,39 @@ func makeFullSong(_ m: String, _ musicLength: Int) -> String {
     
     return str
 }
+
+// MARK: - 튜플
+// https://programmers.co.kr/learn/courses/30/lessons/64065
+func tuple(_ s: String) -> [Int] {
+    var intArrArr = [[Int]]()
+    var s = s
+    s.removeFirst()
+    s.removeLast()
+    
+    for numberString in s.components(separatedBy: "},{") {
+        let numberString = numberString
+            .replacingOccurrences(of: "{", with: "")
+            .replacingOccurrences(of: "}", with: "")
+        let numbers = numberString.split(separator: ",")
+        var intArr = [Int]()
+        for number in numbers {
+            intArr.append(Int(number)!)
+        }
+        intArrArr.append(intArr)
+    }
+    
+    intArrArr.sort {
+        $0.count < $1.count
+    }
+    
+    var intArr = [Int]()
+    
+    intArrArr.forEach {
+        $0.forEach {
+            if !intArr.contains($0) { intArr.append($0) }
+        }
+    }
+    
+    
+    return intArr
+}
